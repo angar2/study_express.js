f7b4ed75c131499ff8960842332da3110458ae10
앵가리 <>
2023-01-11 18:48:00 +0900

    express로 변경: home

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const fs = require('fs');
+const template = require('./lib/template.js');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (request, response) => {
+    fs.readdir(`./data`, function(error, filelist) {
+        var title = 'Welcome';
+        var description = 'Hello Express';
+        var list = template.list(filelist);
+        var HTML = template.HTML(title, list, `<h2>${title}</h2><p>${description}</p>`, `<a href="/create">Create</a>`);
+        response.send(HTML);
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 });
