5457eca79326286bbc014c6e595136436436a209
앵가리 <>
2023-01-11 18:50:46 +0900

    express로 변경: 상세페이지 url 방식 변경: 쿼리스트링->url패스

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const fs = require('fs');
+const path = require('path');
+const sanitizeHTML = require('sanitize-html');
 const template = require('./lib/template.js');
 
 app.get('/', (request, response) => {
@@ -11,6 +13,28 @@ app.get('/', (request, response) => {
         var list = template.list(filelist);
         var HTML = template.HTML(title, list, `<h2>${title}</h2><p>${description}</p>`, `<a href="/create">Create</a>`);
         response.send(HTML);
+    });
+});
+
+app.get('/page/:pageId', (request, response) => {
+    fs.readdir(`./data`, function(error, filelist) {
+        var filteredId = path.parse(request.params.pageId).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', function(error, description){
+            var title = request.params.pageId;
+            var sanitizedTitle = sanitizeHTML(title);
+            var sanitizedDescription = sanitizeHTML(description);
+            var list = template.list(filelist);
+            var HTML = template.HTML(sanitizedTitle, list,
+                `<h2>${sanitizedTitle}</h2><p>${sanitizedDescription}</p>`,
+                `<a href="/create">Create</a>
+                <a href="/update?id=${sanitizedTitle}">Update</a>
+                <form action="delete_process" method="post">
+                    <input type="hidden" name="id" value="${sanitizedTitle}" />
+                    <input type="submit" value="Delete" />
+                </form>`
+            );
+            response.send(HTML);
+        });
     });
 });
 
